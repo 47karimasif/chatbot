3b2f60940971eff788ade9598a55e2bd36f2f3f6
47karimasif <karimasif98@gmail.com>
2020-04-21 03:54:17 +0530

    final-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import CustomChatbot from "../src/components/chatbot/CustomChatbot";
+
+function App(props) {
+  return (
+    <div>
+      <CustomChatbot />
+    </div>
+  );
+}
+
+export default App;
